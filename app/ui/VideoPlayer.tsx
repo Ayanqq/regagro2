d36eee345c09a8c1348c4f23385bcf6ce3de1d10
Ayanqq <nazbievayan16@gmail.com>
2025-08-26 00:00:39 +0500

    paginations

--- app/ui/VideoPlayer.tsx
+++ app/ui/VideoPlayer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useState } from "react";
+import {useRef, useState} from "react";
 import YouTube from "react-youtube";
 import Image from "next/image";
 
@@ -24,7 +24,7 @@ export const VideoPlayer = () => {
                 opts={{
                     width: "100%",
                     height: "100%",
-                    playerVars: { controls: 0 },
+                    playerVars: {controls: 0},
                 }}
                 onReady={onReady}
                 className={`absolute top-0 left-0 w-full h-full transition-opacity ${
@@ -36,10 +36,10 @@ export const VideoPlayer = () => {
             {!isPlaying && (
                 <div className="absolute inset-0 z-20">
                     <Image
-                        src="/images/mobilesection/player.jpg"
+                        src="/images/mobilesection/player.png"
                         alt="Custom preview"
                         fill
-                        className="object-cover"
+                        className="object-contain"
                     />
 
                     {/* Кастомная кнопка */}
@@ -47,11 +47,32 @@ export const VideoPlayer = () => {
                         onClick={handlePlay}
                         className="absolute inset-0 flex items-center justify-center"
                     >
-                        <div className="bg-orange-500 w-12 h-12 sm:w-16 sm:h-16 rounded-full flex items-center justify-center shadow-lg hover:scale-105 transition-transform">
+                        <div
+                            className="bg-orange-500 w-12 h-12 sm:w-16 sm:h-16 rounded-full flex items-center justify-center shadow-lg hover:scale-105 transition-transform">
                             {/* Play-иконка */}
-                            <div className="w-0 h-0 border-l-[10px] sm:border-l-[14px] border-l-white border-t-[6px] sm:border-t-[8px] border-t-transparent border-b-[6px] sm:border-b-[8px] border-b-transparent ml-1"></div>
+                            <div
+                                className="w-0 h-0 border-l-[10px] sm:border-l-[14px] border-l-white border-t-[6px] sm:border-t-[8px] border-t-transparent border-b-[6px] sm:border-b-[8px] border-b-transparent ml-1"></div>
                         </div>
                     </button>
+                    <div className={'absolute md:top-[200px] md:left-[320px] top-[130px] left-[210px] hidden md:block'}>
+                        <svg width="33" height="51" viewBox="0 0 33 51" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M27.5 50C40.5 35 23.5562 25.1055 16.9808 29.2217C10.4053 33.338 15.0745 42.5276 7.44618 42.6104C-0.181986 42.6933 -3.72341 28.8453 11.7315 22.765C19.3554 19.7657 20.3157 15.127 19.8842 11.4364C19.4527 7.74577 14.2658 3.77277 11.4686 2.36459M11.4686 2.36459C11.4686 2.36459 15.5625 1.80172 17.3129 1.46543M11.4686 2.36459C11.4686 2.36459 12.962 6.67594 13.3018 7.96026"
+                                stroke="white" stroke-miterlimit="16" stroke-linecap="round"/>
+                        </svg>
+                    </div>
+                    <div className={'absolute md:top-[210px] md:left-[340px] top-[110px] left-[180px] block md:hidden'}>
+                        <svg width="19" height="29" viewBox="0 0 19 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15.6017 28.1622C22.7589 19.904 13.4305 14.4566 9.8104 16.7228C6.1903 18.989 8.76093 24.0483 4.56119 24.0939C0.361515 24.1396 -1.58824 16.5156 6.92044 13.1681C11.1177 11.5168 11.6464 8.96303 11.4089 6.93118C11.1713 4.89931 8.31567 2.71199 6.7757 1.93672M6.7757 1.93672C6.7757 1.93672 8.84661 1.26952 9.81028 1.08437M6.7757 1.93672C6.7757 1.93672 7.66829 4.87734 7.85538 5.58442" stroke="white" stroke-width="0.5" stroke-miterlimit="16" stroke-linecap="round"/>
+                        </svg>
+                    </div>
+                    <div className={'absolute md:max-w-[215px] max-w-[145px] top-[250px] md:left-[350px] md:leading-[12px] leading-[10px] hidden md:block'}><span
+                        className={'font-normal md:font-medium md:text-[12px] text-[10px] text-[#FFFFFF]'}>watch the short video to find out the epassport features and opportunities</span>
+                    </div>
+                    <div className={'absolute md:max-w-[215px] max-w-[145px] top-[140px] left-[180px] md:top-[260px] md:left-[370px] md:leading-[12px] leading-[10px] block md:hidden'}><span
+                        className={'font-normal md:font-medium md:text-[12px] text-[10px] text-[#FFFFFF]'}>watch the short video to learn more about the platform</span>
+                    </div>
                 </div>
             )}
         </div>
