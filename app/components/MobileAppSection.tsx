c783d8e630dde205dc5ec682956d3befbe879403
Ayanqq <nazbievayan16@gmail.com>
2025-08-26 17:33:27 +0500

    fixes

--- app/components/MobileAppSection.tsx
+++ app/components/MobileAppSection.tsx
@@ -26,7 +26,7 @@ export default function MobileAppSection({sections, setCurrentSection, currentSe
                     <div className={'w-[246px] md:w-auto'}>
                         <Headtitle minTitle={'it solutions'} title={'Mobile app for pets and livestock'}/>
                     </div>
-                    <div className={'flex justify-between md:mt-[30px] mt-[10px]'}>
+                    <div className={'flex justify-between md:mt-[20px] mt-[10px]'}>
                         <div>
                             <div className={'max-w-[650px] flex flex-col gap-[20px]'}>
                                 <div className={'md:block hidden'}><VideoPlayer text={'watch the short video to find out the epassport features and opportunities'}/></div>
@@ -42,7 +42,7 @@ export default function MobileAppSection({sections, setCurrentSection, currentSe
                             </div>
                             <div className={'md:hidden block mt-[15px]'}><VideoPlayer text={'watch the short video to find out the epassport features and opportunities'}/></div>
                             <button
-                                className="md:text-[17px] text-[12px] cursor-pointer mt-[30px] bg-[linear-gradient(90deg,#FFDA18_0%,#FFE55E_100%)] md:px-[60px] md:py-[24px] py-[18px] w-fit rounded-full md:font-bold md:w-auto w-full font-bold text-[#355332]">LEARN
+                                className="md:text-[17px] text-[12px] cursor-pointer mt-[30px] bg-[linear-gradient(90deg,#FFDA18_0%,#FFE55E_100%)] md:px-[60px] md:py-[24px] py-[18px] w-fit rounded-full md:font-  bold md:w-auto w-full font-bold text-[#355332]">LEARN
                                 MORE ABOUT THE
                                 E-PASSPORT
                             </button>
